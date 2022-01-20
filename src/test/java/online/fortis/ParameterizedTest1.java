86677f1b75a8aac81d4852857badb4f410c61fc2
Bigwatch86 <bigwatch86@gmail.com>
2022-01-20 18:19:49 +0300

    added tests

---
+++ src/test/java/online/fortis/ParameterizedTest1.java
@@ -0,0 +1,71 @@
+package online.fortis;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class ParameterizedTest1 {
+
+    @BeforeAll
+    @DisplayName("Открываем Гисметео")
+    static void openPage() throws InterruptedException {
+        open("https://www.gismeteo.ru/");
+        TimeUnit.SECONDS.sleep(3);
+    }
+
+    //задисейбленный тест
+    @Test
+    @Disabled
+    @DisplayName("Задисейбленный тест :-)")
+    void disabledTest(){
+        System.out.println("Задисейбленный тест :-)");
+    }
+
+    //обычный тест с параметрами (testData)
+    @ValueSource(strings = {"Ставрополь", "Нарьян-Мар", "Сочи"})
+    @ParameterizedTest(name = "Обычный тест с параметром: {0}")
+    void testWithParams(String testData) {
+        $("[class=\"input js-input\"]").setValue(testData);
+        $$("[class=\"search-item list-item icon-menu icon-menu-gray\"]").first().click();
+        $(".breadcrumbs-links > .breadcrumbs-link:nth-child(3)").shouldHave(text(testData));
+    }
+
+    //тест с параметрами CSV (testData и expectedResult)
+    @CsvSource(value = {
+            "Ставрополь, Ставропольский край",
+            "Нарьян-Мар, Ненецкий автономный округ",
+            "Сочи, Краснодарский край"
+    })
+    @ParameterizedTest(name = "тест с двумя параметрами {0} > {1}")
+    void testWithCSVParams(String testData, String expectedResult) {
+        $("[class=\"input js-input\"]").setValue(testData);
+        $$("[class=\"search-item list-item icon-menu icon-menu-gray\"]").first().click();
+        $(".breadcrumbs-links > .breadcrumbs-link:nth-child(2)").shouldHave(text(expectedResult));
+    }
+
+
+    //тест с параметрами MethodSource
+
+    static Stream<Arguments> commonSearchTestDataProvider(){
+        return Stream.of(
+                Arguments.of("Москва", "Москва (город федерального значения)"),
+                Arguments.of("Санкт-Петербург", "Санкт-Петербург (город федерального значения)")
+        );
+    }
+    @MethodSource("commonSearchTestDataProvider")
+    @ParameterizedTest(name = "тест с параметрами {0} > {1}")
+        void testWithMethodSource(String testData, String expectedResult) {
+            $("[class=\"input js-input\"]").setValue(testData);
+            $$("[class=\"search-item list-item icon-menu icon-menu-gray\"]").first().click();
+            $(".breadcrumbs-links > .breadcrumbs-link:nth-child(2)").shouldHave(text(expectedResult));
+        }
+}
